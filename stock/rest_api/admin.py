95ec3d0f416ea0f895cdae2c78e49518f968aee7
Ryu0n <32003817+Ryu0n@users.noreply.github.com>
2020-12-04 18:02:39 +0900

    -
    -

--- stock/rest_api/admin.py
+++ stock/rest_api/admin.py
@@ -1,5 +1,3 @@
-from .models import User
 from django.contrib import admin
 
 # Register your models here.
-admin.site.register(User)
--- stock/stock/settings.py
+++ stock/stock/settings.py
@@ -37,7 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stock_inquiry',
+    'stock_prediction',
+    'rest_framework',
+    'rest_api'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
