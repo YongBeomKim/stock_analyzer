8a785e56de316d49ecc70b109781669e4c4e498c
Ryu0n <32003817+Ryu0n@users.noreply.github.com>
2020-12-28 23:08:04 +0900

    using @material-ui/core

--- react-app/src/components/inquiry/card/cardStockMarket.js
+++ react-app/src/components/inquiry/card/cardStockMarket.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import {Card, CardHeader, CardContent, CardActions} from '@material-ui/core'
 
 class CardStockMarket extends Component {
     render() {
         var marketName = this.props.name;
         
+        return (
+            <Card>
+                <CardHeader></CardHeader>
+                <CardContent></CardContent>
+                <CardActions></CardActions>
+            </Card>
+        );
     }
   }
 
