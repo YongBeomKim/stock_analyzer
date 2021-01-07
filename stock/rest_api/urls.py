d8c9820dc4f0ef4b7b97109f50cca8e067e31325
Ryu0n <32003817+Ryu0n@users.noreply.github.com>
2021-01-07 23:04:56 +0900

    Add comment about DRF URL.
    DRF URL - DefaultRouter에 의해 생성되는 URL 패턴 분석 완료.

--- stock/rest_api/urls.py
+++ stock/rest_api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import StockItemCollector
 from .views import StockUserViewSet, StockMarketViewSet, StockItemViewSet
 
 router_user = DefaultRouter()
--- stock/rest_api/views.py
+++ stock/rest_api/views.py
@@ -1,11 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import viewsets 
 
 from .serializers import StockUserSerializer, StockMarketSerializer, StockItemSerializer
 from .models import StockUser, StockMarket, StockItem
 
+
+# ModelViewSet URL
+# DefaultRouter를 사용하여 해당 URL들이 자동으로 생성.
+
+# rest_api/user/
+# post_list = PostViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create',
+# })
+
+# rest_api/user/{id}/
+# post_detail = PostViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy',
+# })
 
 # Create your views here.
 class StockUserViewSet(viewsets.ModelViewSet):
