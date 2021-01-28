7f5351a610eb1d5bd1d45e200683ac4c90b3b39b
Ryu0n <32003817+Ryu0n@users.noreply.github.com>
2021-01-28 11:21:43 +0900

    Update req_interface.py

--- stock/interface/req_interface.py
+++ stock/interface/req_interface.py
@@ -1,31 +1,12 @@
+import abc
 import requests
 
 # http://127.0.0.1:8000/rest_api/user/10/
 
-class ReqBase:
+class BaseReq:
     def __init__(self):
         self.url = 'https://localhost.com/rest_api/'
         self.data = None
-
-    def send_get(self):
-        res = requests.get(self.url)
-        return self._ret(res)
-
-    def send_post(self):
-        res = requests.post(self.url, json=self.data)
-        return self._ret(res)
-
-    def send_put(self):
-        res = requests.put(self.url, json=self.data)
-        return self._ret(res)
-
-    def send_patch(self):
-        res = requests.patch(self.url, json=self.data)
-        return self._ret(res)
-
-    def send_delete(self):
-        res = requests.delete(self.url)
-        return self._ret(res)
 
     @staticmethod
     def _ret(res):
@@ -40,19 +21,38 @@ class ReqBase:
     def get_param(self):
         return self.data
 
+class BaseCreate(BaseReq):
+    def send_post(self):
+        res = requests.post(self.url, json=self.data)
+        return self._ret(res)
 
-class RetrieveItemReq(ReqBase):
+class BaseRead(BaseReq):
+    def send_get(self):
+        res = requests.get(self.url)
+        return self._ret(res)
 
+class BaseUpdate(BaseReq):
+    def send_put(self):
+        res = requests.put(self.url, json=self.data)
+        return self._ret(res)
+
+    def send_patch(self):
+        res = requests.patch(self.url, json=self.data)
+        return self._ret(res)
+
+class BaseDelete(BaseReq):
+    def send_delete(self):
+        res = requests.delete(self.url)
+        return self._ret(res)
+
+class CreateItemReq(BaseCreate):
     pass
 
-
-class DeleteItemReq(ReqBase):
+class ReadItemReq(BaseRead):
     pass
 
-
-class UpdateItemReq(ReqBase):
+class UpdateItemReq(BaseUpdate):
     pass
 
-
-class InsertItemReq(ReqBase):
+class DeleteItemReq(BaseDelete):
     pass
