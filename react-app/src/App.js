2f8558764d0e1bf66d324502d5e4cc92bd121417
ckr <ckr3453@gmail.com>
2021-02-23 19:31:23 +0900

    장고서버로 get 요청구문 추가

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,19 +1,42 @@
 import CardStockMarket from './components/inquiry/card/cardStockMarket';
 import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    // <div className="App" style={style}>
-    <div id="grid">
-      <ul className="stockMarkets">
-        <CardStockMarket></CardStockMarket>
-        <CardStockMarket></CardStockMarket>
-        <CardStockMarket></CardStockMarket>
-        <CardStockMarket></CardStockMarket>
-        <CardStockMarket></CardStockMarket>
-      </ul>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    axios({
+      method : "get",
+      url : "http://127.0.0.1:8000/rest_api/market/"
+    })
+    .then(response => {
+      console.log(response);
+      let stockMarkets = {};
+      response.data.forEach(element => {
+        console.log(element["id"]);
+        console.log(element["stock_market_name"]);
+      });
+    })
+    .catch(error => {
+      console.log("error", error);
+    })
+    return (
+      // <div className="App" style={style}>
+      <div id="grid">
+        <ul className="stockMarkets">
+          <CardStockMarket></CardStockMarket>
+          <CardStockMarket></CardStockMarket>
+          <CardStockMarket></CardStockMarket>
+          <CardStockMarket></CardStockMarket>
+          <CardStockMarket></CardStockMarket>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
