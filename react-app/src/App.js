239dc61dbfa7086ed383f601bb0ce4bc7f2e0ea8
Ryu0n <32003817+Ryu0n@users.noreply.github.com>
2021-02-24 14:44:56 +0900

    Display stock market view.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,55 +4,50 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class App extends Component{
+
+  state = {
+    markets: []
+  }
+
   constructor(props){
-    console.log('constructor')
     super(props);
-    this.markets = [];
-    fetch("http://127.0.0.1:8000/rest_api/market/",{
-      method:"GET"
-    })
-    .then(response => response.json())
-    .then(response => console.log('success', JSON.stringify(response)))
-
-
-    // console.log('after', this.market)
-    
-    // axios({
-    //   method : "get",
-    //   // url : "http://175.116.181.151:4040/siqms/rest_api/getAllLayerIdNameList"
-    //   url : "http://127.0.0.1:8000/rest_api/market/"
-    // })
-    // .then(response => {
-    //   console.log(response);
-    //   response.data.forEach(element => {
-    //     // debugger;
-    //     let _id = element["id"];
-    //     let _name = element["stock_market_name"];
-    //     let market = <CardStockMarket name={_name}/>
-    //     this.markets.push(market);
-    //     console.log('here is ', this.markets);
-    //   });
-    // })
-    // .catch(error => {
-    //   console.log("error", error);
-    // })
-    this.state = {
-      markets: [
-        // {id: 1, title: 'KOSPI'}
-      ]
-    }
   }
 
   render(){
+    console.log('im render!', this.state.markets);
+
     return (
       // <div className="App" style={style}>
       <div id="grid">
         <ul className="stockMarkets">
-          {/* {this.state.markets} */}
-          {this.markets}
+          {this.state.markets}
         </ul>
       </div>
     );
+  }
+
+  componentDidMount(){
+    console.log('im didmount!');
+    axios({
+      method : "get",
+      url : "http://127.0.0.1:8000/rest_api/market/"
+    })
+    .then(response => {
+      console.log(response);
+      let _markets = [];
+      response.data.forEach(element => {
+        // debugger;
+        let _id = element["id"];
+        let _name = element["stock_market_name"];
+        let market = <CardStockMarket name={_name}/>
+        _markets = _markets.concat(market);
+      });
+      console.log('hello', _markets);
+      this.setState({markets: _markets});
+    })
+    .catch(error => {
+      console.log("error", error);
+    })
   }
 }
 
